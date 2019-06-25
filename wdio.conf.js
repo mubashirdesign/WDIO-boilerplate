dfa14c029af10626f19e07fcff0055a5f011cfda
Mubashir Cheentavida <mcheentavida@terrascend.com>
2019-06-25 15:16:06 -0400

    ljklj

--- wdio.conf.js
+++ wdio.conf.js
@@ -101,6 +101,8 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
+    port: 9515, // default for ChromeDriver
+    path: '/',
     services: ['chromedriver'],
     //'sauce','testingbot','firefox-profile',,'devtools','applitools','browserstack','appium','phantomjs','static-server','visual-regression','webpack','webpack-dev-server','chromedriver','iedriver'
     // Framework you want to run your specs with.
